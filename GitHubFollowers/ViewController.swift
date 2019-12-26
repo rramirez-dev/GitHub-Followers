58feb6e2cb7736400d5f5f8d9ed513cf4229c3a3
Robert Ramirez <robertpramirez44@gmail.com>
2019-12-26 12:23:46 -0700

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Robert Ramirez on 12/26/19.
+//  Copyright © 2019 Robert Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
